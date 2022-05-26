9da03e9a7f724006a46520320e48c2f387904bd0
hwseo0705 <julieseo0705@gmail.com>
2022-05-26 17:33:22 +0900

    [05.25.22] Java Coding Practice Commit

---
+++ src/F_String/GroupWordChecker.java
@@ -0,0 +1,34 @@
+package F_String;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class GroupWordChecker {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int count = Integer.parseInt(br.readLine());
+        int groupCount = 0;
+        br.readLine();
+        for (int i = 0; i < count; i++) {
+            String word = br.readLine();
+//            System.out.println(word);
+            ArrayList<Character> wordList = new ArrayList<>();
+            for (int j = 0; j < word.length() - 1; j++) {
+                if (wordList.contains(word.charAt(j + 1))) {
+                    groupCount--;
+                    break;
+                }
+                if (word.charAt(j) == word.charAt(j + 1)) {
+                    continue;
+                } else {
+                    wordList.add(word.charAt(j));
+                }
+            }
+            groupCount++;
+        }
+        System.out.println(groupCount);
+        br.close();
+    }
+}
