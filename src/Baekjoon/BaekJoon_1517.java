87da40820b5b079e6aa87c3823c4a3a1c8dc0933
hwseo0705 <julieseo0705@gmail.com>
2022-06-13 18:18:31 +0900

    [06.13.22] Java Coding Practice Commit - Sorting Problems

---
+++ src/Baekjoon/BaekJoon_1517.java
@@ -0,0 +1,34 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BaekJoon_1517 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        br.readLine();
+
+        int[] A = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+        br.close();
+
+        int count = 0;
+        for (int i = A.length - 1; i > 0 ; i--) {
+            for (int j = 0; j < i; j++) {
+                if (A[j] > A[j + 1]) {
+                    int temp = A[j];
+                    A[j] = A[j + 1];
+                    A[j + 1] = temp;
+                    count++;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
---
+++ src/Programmers/Kth_Num.java
@@ -0,0 +1,26 @@
+package Programmers;
+
+import java.util.Arrays;
+
+public class Kth_Num {
+
+    public static int solution(int[] citations) {
+        int answer = 0;
+        Arrays.sort(citations);
+        System.out.println("Citations: " + Arrays.toString(citations));
+
+        // 0 1 3 5 6
+
+        int H = citations.length;
+        for (int i = 0; i < citations.length; i++) {
+            if (citations[i] < H) H--;
+            else answer = H;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(new int[]{3, 0, 6, 1, 5}));
+    }
+}
