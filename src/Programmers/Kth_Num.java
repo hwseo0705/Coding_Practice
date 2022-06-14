478cfa1d19dbe805589a4b9e241417896e56834b
hwseo0705 <julieseo0705@gmail.com>
2022-06-14 13:34:36 +0900

    [06.14.22] Java Coding Practice Commit - Sorting Problems

--- src/Programmers/Kth_Num.java
+++ src/Programmers/Kth_Num.java
@@ -4,23 +4,21 @@ import java.util.Arrays;
 
 public class Kth_Num {
 
-    public static int solution(int[] citations) {
-        int answer = 0;
-        Arrays.sort(citations);
-        System.out.println("Citations: " + Arrays.toString(citations));
-
-        // 0 1 3 5 6
-
-        int H = citations.length;
-        for (int i = 0; i < citations.length; i++) {
-            if (citations[i] < H) H--;
-            else answer = H;
+    public static int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        int k = 0;
+        for (int i = 0; i < commands.length; i++) {
+            int[] arr = new int[commands[i][1] - commands[i][0] + 1];
+            int index = 0;
+            for (int j = commands[i][0] - 1; j < commands[i][1]; j++) {
+                arr[index++] = array[j];
+            }
+            Arrays.sort(arr); // nlogn quicksort
+            answer[k++] = arr[commands[i][2] - 1];
         }
-
         return answer;
     }
-
     public static void main(String[] args) {
-        System.out.println(solution(new int[]{3, 0, 6, 1, 5}));
+        System.out.println(Arrays.toString(solution(new int[]{1, 5, 2, 6, 3, 7, 4}, new int[][]{{2, 5, 3}, {4, 4, 1}, {1, 7, 3}})));
     }
 }
